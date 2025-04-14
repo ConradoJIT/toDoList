793ce14b7f37b39240cb3fdf9bb887dc5fa92738
gabrieldiasjit <gabrieldiasjittechnology@gmail.com>
2025-04-14 10:04:59 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = 5000;
